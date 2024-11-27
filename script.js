96c7dd5c2c52699858b7af274c6e4314e6f7852f
juandimg <61071152+juandimg@users.noreply.github.com>
2024-11-26 22:21:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ function showSlides(n) {
         slide.style.transform = `translateX(-${slideIndex * 100}%)`;
     });
 }
+
+
+function initMap() {
+    var location = { lat: -34.397, lng: 150.644 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: location
+    });
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+  }
+  window.onload = initMap;
